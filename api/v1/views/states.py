b3e20d1358967b117ca3efbd49ee7912c7538c4a
John Nyokabi <jwnyokabi@gmail.com>
2023-05-05 20:45:09 +0300

    RESTful API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,27 +6,28 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET', 'POST'])
-def all_states():
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+def all_states(state_id=None):
     """Defines GET and POST methods for route"""
     state_obj = storage.all(State)
 
     states = [obj.to_dict() for obj in state_obj.values()]
-    if request.method == 'GET':
-        return jsonify(states)
-    elif request.method == 'POST':
-        res_dict = request.get_json()
+    if not state_id:
+        if request.method == 'GET':
+            return jsonify(states)
+        elif request.method == 'POST':
+            res_dict = request.get_json()
 
-        if res_dict is None:
-            abort(400, "Not a JSON")
-        if res_dict.get("name") is None:
-            abort(400, "Missing name")
-        state_dict = State(**res_dict)
-        state_dict.save()
-        return jsonify(state_dict.to_dict()), 201
+            if res_dict is None:
+                abort(400, "Not a JSON")
+            if res_dict.get("name") is None:
+                abort(400, "Missing name")
+            state_dict = State(**res_dict)
+            state_dict.save()
+            return jsonify(state_dict.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['GET', 'PUT', 'DELETE'])
+@app_views.route('/states/<state_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
 def state_id(state_id):
     """updates the states object"""
     state_obj = storage.get('State', state_id)
