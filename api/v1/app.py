4a813f2402982d8e348b751f7e53cb47144d10ff
John Nyokabi <jwnyokabi@gmail.com>
2023-05-05 13:26:27 +0300

    RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,16 +5,26 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exception):
     """Removes current SQLAlchemy session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def notFound(error):
+    """Returns status of the 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
